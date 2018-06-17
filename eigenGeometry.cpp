b8e4ace40809c1ecb019c4f697e58aeecfbfe14c
Daviddsun <intermin@163.com>
2018-06-17 14:45:04 +0800

    0

--- eigenGeometry.cpp
+++ eigenGeometry.cpp
@@ -14,6 +14,7 @@ int main(int argc,char** argv)
     Eigen::Matrix3d rotation_matrix = Eigen::Matrix3d::Identity();
     Eigen::AngleAxisd rotation_vector(M_PI/4,Eigen::Vector3d(0,0,1));
     cout .precision(8);
-    cout<<"rotation matrix = \n"<<rotation_vector.matrix()<<endl;
+    cout<<"rotation matrix 0 = \n"<<rotation_vector.matrix()<<endl;
     return 0;
+
 }
